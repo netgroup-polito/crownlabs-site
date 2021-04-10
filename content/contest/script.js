9dda58590f97fcb780560689bff94b4ff8b5439a
Alessandro Cannarella <cannarella.dev@gmail.com>
2021-04-10 10:26:48 +0200

    added POST votation and vote check by localStorage

--- content/contest/script.js
+++ content/contest/script.js
@@ -5,59 +5,90 @@ let vote = document.getElementById('vote');
 let terms = document.getElementById('terms');
 let email = document.getElementById('email');
 
-videos.forEach(video => {
-    video.addEventListener('click', event => {
-        if(ActiveSelection){
-            ActiveSelection.classList.remove('btn-success');
-            ActiveSelection.classList.add('btn-primary');
-            ActiveSelection.innerHTML = "Select";
-        }
-        event.target.classList.remove('btn-primary');
-        event.target.classList.add('btn-success');
-        event.target.innerHTML = "Selected";
-        ActiveSelection = event.target;
-    })
-})
+const storage = JSON.parse(localStorage.getItem('data'));
 
-vote.addEventListener('click', event => {
-    let errorMessage = "Ops! Something went wrong:<br>";
-    var mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-    let error = false;
-    if(!ActiveSelection){
-        errorMessage += "<li>Select a Concept to vote</li>";
-        error = true;
+selection = (event) => {
+    if(ActiveSelection){
+        ActiveSelection.classList.remove('btn-success');
+        ActiveSelection.classList.add('btn-primary');
+        ActiveSelection.innerHTML = "Select";
     }
-    /*if(!email.value.match(mailformat)){
-        errorMessage += "<li>Insert a Valid email</li>";
-        error = true;
-    }
-    if(!terms.checked){
-        errorMessage += "<li>Accept our Terms</li>";
-        error = true;
-    }*/
-    if(error){
-        result.innerHTML = `
-        <div class="col-12 alert alert-danger" role="alert">
-            ${errorMessage}
-        </div>
-        `
-        result.classList.add('show');
-    }else{
-        /** INSERT HERE CALL TO ACTION GOOGLE FORM
-         * @parameter to send
-         * activeSelection.value    [NUMBER]
-         * emaile.value             [EMAIL]
-         */
-        sendVote();       
-    }
-})
+    event.target.classList.remove('btn-primary');
+    event.target.classList.add('btn-success');
+    event.target.innerHTML = "Selected";
+    ActiveSelection = event.target;
+    vote.disabled = false;
+}
 
 sendVote = async () => {
+
+    // Get vote Date
+    const date = new Date();
+
+    // Get the Selected Vote
+    const voted = ActiveSelection.value;
+
+    // Get the User IP
     const userIP = await fetch('https://api64.ipify.org?format=json').then(response => response.json()).then(data => {return data.ip});
-    result.innerHTML = `
-    <div class="col-12 alert alert-success" role="alert">
-        Well Done! Thank you for your vote ${userIP}
-    </div>
-    `
+    
+    // POST Vote on Spreadsheet
+    let resultVotation = await fetch("https://api.apispreadsheets.com/data/10703/", {
+    method: "POST",
+    body: JSON.stringify({"data": {"vote":voted,"userIP":userIP,"date":date}})})
+
+    // Print the result alert
+    if(resultVotation.status === 201){
+        result.innerHTML = `
+            <div class="col-12 alert alert-success" role="alert">
+                Well Done! Thank you for your vote
+            </div>
+            `
+            // Save vote Obj in localStorage
+            const data = {
+                'submit': true,
+                'value': voted,
+                'IP': userIP,
+                'date': date
+            }
+            localStorage.setItem('data', JSON.stringify(data));
+    }else{
+        result.innerHTML = `
+            <div class="col-12 alert alert-danger" role="alert">
+                Error: ${resultVotation.status}
+            </div>
+            `
+    }
+
+    // Show Result Alert
     result.classList.add('show');
+
+    // Test Result
+    console.log(resultVotation);
+
+    // Disable repeated Vote
+    vote.disabled = true;
+
+    // Disable event listener selection
+    videos.forEach(video => {
+        video.removeEventListener('click', selection)
+
+    })
+}
+
+if (storage){
+    result.innerHTML = `
+        <div class="col-12 alert alert-danger" role="alert">
+            You have already vote Concept ${storage.value}
+        </div>
+        `
+    // Show Result Alert
+    result.classList.add('show');
+
+}else{
+
+    videos.forEach(video => {
+        video.addEventListener('click', selection)
+    });
+    
+    vote.addEventListener('click', sendVote);
 }
